521aa2c5152e44e2be383f474e8bc0305bf5e7a4
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-01-19 20:36:00 +0100

    Update avidina_numero.py

--- Programas/avidina_numero.py
+++ Programas/avidina_numero.py
@@ -6,4 +6,16 @@ Proporciona pistas como "mayor" o "menor". Termina cuando el usuario acierte.
 
 import random
 
+num_secreto = random.randint(1, 100)
 
+numero = int(input("Introduzca un número entre 1 y 100: "))
+
+while numero != num_secreto:
+    if numero < num_secreto:
+        print(f"El número secreto es mayor a {numero}")
+        numero = int(input("Introduce otro número: "))
+    else:
+        print(f"El número secreto es menor a {numero}")
+        numero = int(input("Introduce otro número: "))
+        
+print("FELICIDADES!!")
