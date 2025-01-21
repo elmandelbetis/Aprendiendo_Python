4d626d27f1a75ebf6ef9f1807c0bd6b62f6ff27b
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-01-21 14:26:29 +0100

    yea

--- Programas/generador_aleatorio_enteros.py
+++ Programas/generador_aleatorio_enteros.py
@@ -11,16 +11,22 @@ lista_aleatorios = []
 
 cota_min = int(input("Introduzca el rango mínimo: "))
 cota_max = int(input("Introduzca el rango máximo: "))
-# num_elementos = int(input("Introduzca el número de elementos de la lista: "))
 
 
-for i in range (0, num_elementos):
+while cota_min >= cota_max:
+    print("Por favor, introduzca un rango válido.")
+    cota_min = int(input("Introduzca el rango mínimo: "))
+    cota_max = int(input("Introduzca el rango máximo: "))
+
+
+for i in range (cota_min, cota_max):
     num_random = random.randint(cota_min, cota_max)
     
-    while(num_random in lista_aleatorios):
+    while num_random in lista_aleatorios:
         num_random = random.randint(cota_min, cota_max)
     
     lista_aleatorios.append(num_random)
 
 
-print(f"Lista de aleatorios: {lista_aleatorios}")
+print("Lista de aleatorios:")
+print(lista_aleatorios)
