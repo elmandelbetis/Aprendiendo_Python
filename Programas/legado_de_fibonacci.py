a5c216ff652d47c2d0f7dd504ab2a4114a2a8384
Álvaro Maldonado Medina <126078785+elmandelbetis@users.noreply.github.com>
2025-01-24 18:28:35 +0100

    la recursividad en python es una mierda

--- Programas/legado_de_fibonacci.py
+++ Programas/legado_de_fibonacci.py
@@ -3,8 +3,25 @@
 El legado de Fibonacci
 "CON EL PODER DEL BALONCESTO (-ESTOO) (-ESTOOOO)!!!!" - Mr Jägger
 """
+import time
 
-def fibonacci(termino):
+
+def fibonacci_lineal(termino):
+    # Casos base
+    if termino == 0:
+        return 0
+    elif termino == 1:
+        return 1
+    
+    a = 0
+    b = 1
+
+    for i in range (2, n+1):
+        a, b = b, a + b
+    
+    return b
+    
+def fibonacci_recursivo(termino):
     
     # Casos base
     if termino == 0:
@@ -13,13 +30,23 @@ def fibonacci(termino):
         return 1
     
     # Caso recursivo
-    return fibonacci(termino - 1) + fibonacci(termino - 2)
+    return fibonacci_recursivo(termino - 1) + fibonacci_recursivo(termino - 2)
 
 
 
 # Programa principal
 
 n = int(input("Introduzca el término f(n) a calcular de la sucesión: "))
-print(f"El valor de la sucesión de Fibonacci para f({n}) es: {fibonacci(n)}")
-    
+
+start = time.time()
+print(f"Fibonacci f({n}) lineal: {fibonacci_lineal(n)}")
+end = time.time()
+print(f"Tiempo transcurrido: {end - start} segundos")
+
+
+start = time.time()
+print(f"Fibonacci f({n}) recursivo: {fibonacci_recursivo(n)}")
+end = time.time()
+print(f"Tiempo transcurrido: {end - start} segundos")
+
 
